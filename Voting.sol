2eb71120d6262a64313152fd140d6437d85e2a53
Eric Davis <ericmdavis4@gmail.com>
2018-09-04 23:11:08 -0500

    add events for frontend

--- Voting.sol
+++ Voting.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.4.18;
 
 contract Voting {
   enum VoteStates {Absent, Yes, No}
+  event VoteCreated(uint256 pollNumber);
+  event VoteCast(uint256 voteNumber);
   Vote[] public votes;
   struct Vote {
     address creator;
@@ -10,9 +12,9 @@ contract Voting {
     uint256 no;
     mapping (address => VoteStates) voterStates;
   }
-  
   function newVote(string _question) external {
     votes.push(Vote(msg.sender, _question, 0, 0));
+    VoteCreated(votes.length-1);
   }
   function castVote(uint256 _id, bool _vote) public {
     Vote storage thisVote = votes[_id];
@@ -20,26 +22,32 @@ contract Voting {
     if (_vote == true && state == VoteStates.Absent) {
       thisVote.yes++;
       thisVote.voterStates[msg.sender] = VoteStates.Yes;
+      VoteCast(_id);
     }
     else if (_vote == true && state == VoteStates.No) {
       thisVote.yes++;
       thisVote.no--;
       thisVote.voterStates[msg.sender] = VoteStates.Yes;
+      VoteCast(_id);
     }
     else if (_vote == false && state == VoteStates.Absent) {
       thisVote.no++;
       thisVote.voterStates[msg.sender] = VoteStates.No;
+      VoteCast(_id);
     }
     else if (_vote == false && state == VoteStates.Yes) {
       thisVote.yes--;
       thisVote.no++;
       thisVote.voterStates[msg.sender] = VoteStates.No;
+      VoteCast(_id);
     }
     else if (_vote == true && state == VoteStates.Yes) {
       thisVote.voterStates[msg.sender] = VoteStates.Yes;
+      VoteCast(_id);
     }
     else if (_vote == false && state == VoteStates.No) {
       thisVote.voterStates[msg.sender] = VoteStates.No;
+      VoteCast(_id);
     }
   }
 }
