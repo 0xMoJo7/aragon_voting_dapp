d20d51fa712a3f88e28142a3af6af508c6f92aed
Eric Davis <ericmdavis4@gmail.com>
2018-09-04 22:23:57 -0500

    handle & allow people to change their vote

--- Voting.sol
+++ Voting.sol
@@ -1,23 +1,45 @@
 pragma solidity ^0.4.18;
 
 contract Voting {
+  enum VoteStates {Absent, Yes, No}
   Vote[] public votes;
   struct Vote {
     address creator;
     string question;
     uint256 yes;
     uint256 no;
+    mapping (address => VoteStates) voterStates;
   }
+  
   function newVote(string _question) external {
     votes.push(Vote(msg.sender, _question, 0, 0));
   }
-  function castVote(uint256 _id, bool _vote) {
+  function castVote(uint256 _id, bool _vote) public {
     Vote storage thisVote = votes[_id];
-    if (_vote == true) {
+    VoteStates state = thisVote.voterStates[msg.sender];
+    if (_vote == true && state == VoteStates.Absent) {
       thisVote.yes++;
+      thisVote.voterStates[msg.sender] = VoteStates.Yes;
     }
-    if (_vote == false) {
+    else if (_vote == true && state == VoteStates.No) {
+      thisVote.yes++;
+      thisVote.no--;
+      thisVote.voterStates[msg.sender] = VoteStates.Yes;
+    }
+    else if (_vote == false && state == VoteStates.Absent) {
       thisVote.no++;
-    }  
+      thisVote.voterStates[msg.sender] = VoteStates.No;
+    }
+    else if (_vote == false && state == VoteStates.Yes) {
+      thisVote.yes--;
+      thisVote.no++;
+      thisVote.voterStates[msg.sender] = VoteStates.No;
+    }
+    else if (_vote == true && state == VoteStates.Yes) {
+      thisVote.voterStates[msg.sender] = VoteStates.Yes;
+    }
+    else if (_vote == false && state == VoteStates.No) {
+      thisVote.voterStates[msg.sender] = VoteStates.No;
+    }
   }
 }
